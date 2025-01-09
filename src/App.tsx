fde72c5bfb6bd9dd5b49b8328a4debf15bf89913
AmithBV0606 <amithrao0606@gmail.com>
2025-01-09 13:07:50 +0530

    Added routing using react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
 function App() {
   return (
-    <div>
-      <div>
-        <h1></h1>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={"/"}
+          element={
+            <div className="flex justify-center items-center h-screen">
+              <h1 className="text-4xl font-medium">
+                Welcome to react-hook-form tutorial
+              </h1>
+            </div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
